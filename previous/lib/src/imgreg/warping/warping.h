22b2f6e6a4f057fc0ed2e12baa8868160144049d
Nikita <67832881+NikitaS-19@users.noreply.github.com>
2020-07-16 21:18:42 +0300

    Update warping.h

--- previous/lib/src/imgreg/warping/warping.h
+++ previous/lib/src/imgreg/warping/warping.h
@@ -1,10 +1,17 @@
 #pragma once
 
 #include <opencv2/core/mat.hpp>
+#include <iostream>
 
 class Warping {
  public:
   virtual cv::Mat getWarping(const cv::Mat &img1,
                              const cv::Mat &img2,
                              const cv::Mat &homography) const = 0;
+
+  virtual void init() = 0;
+
+  virtual void free() = 0;
+  
+
 };
