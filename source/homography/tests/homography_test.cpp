66d0b26f2da7d6e2d22341edda3cc2d6dfd1b395
Roman Grigorev <romanfixture@gmail.com>
2020-08-04 20:50:24 +0300

    Add test for homography

---
+++ source/homography/tests/homography_test.cpp
@@ -0,0 +1,25 @@
+#include "../include/stitching/homography/homography.h"
+
+#include <gtest/gtest.h>
+
+#include <opencv2/core/types.hpp>
+#include <opencv2/features2d.hpp>
+#include <opencv2/imgcodecs.hpp>
+
+//using namespace stitching::homography;
+
+namespace {
+	
+TEST(Homography_Test, output_Homographies_Test) {
+
+  std::vector<cv::Point2f> Points_1;
+  std::vector<cv::Point2f> Points_2;
+
+  auto homography = cv::makePtr<stitching::homography::Homography>();
+
+  std::vector<cv::Mat> homographies;
+  
+  EXPECT_THROW(homography->exec(Points_1, Points_2, homographies), std::invalid_argument);
+  
+}
+}  // namespace
