ab8947c62101992057bed2632d6d4db9061bf917
Svetuf <ilucha556@gmail.com>
2020-08-06 20:20:33 +0300

    Add tests for matching

--- source/matching/src/knn_matching.cpp
+++ source/matching/src/knn_matching.cpp
@@ -7,22 +7,26 @@ namespace stitching::matching {
 
 void KnnMatching::init() { matcher = cv::DescriptorMatcher::create(matcherType); }
 
-std::vector<cv::DMatch> KnnMatching::exec(cv::Ptr<core::IFeatures> features1,
-                                                cv::Ptr<core::IFeatures> features2,
-                                                int               k) const {
-  std::vector<std::vector<cv::DMatch>> knnMatches;
+std::vector<cv::DMatch> KnnMatching::exec(cv::Ptr<core::IFeatures> features1, cv::Ptr<core::IFeatures> features2, int k) const
+{
 
-  matcher->knnMatch(features1->descriptors, features2->descriptors, knnMatches, k);
-
-  std::vector<cv::DMatch> matches{};
-
-  for (auto& knnMatch : knnMatches) {
-    if (knnMatch[0].distance < ratio * knnMatch[1].distance) {
-      matches.push_back(knnMatch[0]);
+    if (features1.empty() || features2.empty()) {
+         throw std::invalid_argument(std::string("Empty features!"));
     }
-  }
 
-  return matches;
+    std::vector<std::vector<cv::DMatch>> knnMatches;
+
+    matcher->knnMatch(features1->descriptors, features2->descriptors, knnMatches, k);
+
+    std::vector<cv::DMatch> matches{};
+
+    for (auto& knnMatch : knnMatches) {
+      if (knnMatch[0].distance < ratio * knnMatch[1].distance) {
+        matches.push_back(knnMatch[0]);
+      }
+    }
+
+    return matches;
 }
 
 void KnnMatching::free() { matcher.release(); }
---
+++ source/matching/tests/matching_test.cpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <gtest/gtest.h>
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/cvstd.hpp>
+
+#include "../include/stitching/matching/knn_matching.h"
+
+namespace {
+
+	TEST(Matching_Test, output_Matching_Test) {
+  
+	  cv::Ptr<core::IFeatures> features1;
+	  cv::Ptr<core::IFeatures> features2;
+
+	  auto matching = cv::makePtr<stitching::matching::KnnMatching>();
+
+	  EXPECT_THROW(matching->exec(features1, features2, 0), std::invalid_argument);
+	}
+} 
