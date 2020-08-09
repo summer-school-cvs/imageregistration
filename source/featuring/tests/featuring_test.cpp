e9ddd8ffb5e2c132e55b6b73e9c68a59b08f01cf
haumiriff <haumiriff@yandex.ru>
2020-08-09 14:32:24 +0300

    Add tests for empty output

--- source/featuring/tests/featuring_test.cpp
+++ source/featuring/tests/featuring_test.cpp
@@ -1,51 +1,38 @@
-#include <orb/orb_featuring.h>
-#include <surf/surf_featuring.h>
-
 #include <gtest/gtest.h>
-#include <gtest/internal/gtest-type-util.h>
+#include <stitching/featuring/orb/orb_featuring.h>
+#include <stitching/featuring/surf/surf_featuring.h>
 
 #include <filesystem>
-
 #include <opencv2/imgcodecs.hpp>
 
+using namespace stitching::featuring;
 
 namespace {
 
-    template <class T>
-    class FeaturingTest : public testing::Test {
-    protected:
-        FeaturingTest(): featuring(dynamic_cast<T* const>(new T())) {};
+template <class T>
+class FeaturingTest : public testing::Test {
+ protected:
+  void SetUp() override { featuring.init(); }
+  void TearDown() override { featuring.free(); }
 
-        ~FeaturingTest() override {featuring->free();};
+  T                           featuring;
+  const std::filesystem::path test_dir = TEST_DATA_DIR;
+};
 
-        T* const featuring;
-    };
+using Implementations = testing::Types<OrbFeaturing, SurfFeaturing>;
 
+TYPED_TEST_SUITE(FeaturingTest, Implementations);
 
-    using Implementations = testing::Types<OrbFeaturing, SurfFeaturing>;
+TYPED_TEST(FeaturingTest, NoFeatures) {
+  EXPECT_THROW(this->featuring.exec(cv::Mat{}), std::invalid_argument);
+}
 
-    TYPED_TEST_SUITE(FeaturingTest, Implementations);
+TYPED_TEST(FeaturingTest, WithFeatures) {
+  auto image = cv::imread((this->test_dir / "images" / "img2_1.jpg").string());
 
-    TYPED_TEST(FeaturingTest, NoFeatures) {
+  auto features = this->featuring.exec(image);
 
-        this->featuring->init();
-        auto empty_image = cv::imread(std::string());
-
-        EXPECT_THROW(this->featuring->exec(empty_image), std::invalid_argument);
-    }
-
-
-    TYPED_TEST(FeaturingTest, WithFeatures) {
-
-        this->featuring->init();
-        auto data_path = std::filesystem::current_path() ;
-        auto image  = cv::imread(data_path / "pics/img2_1.jpg");
-
-        EXPECT_NO_THROW(this->featuring->exec(image));
-    }
+  EXPECT_FALSE(features->keyPoints.empty());
+}
 
 }  // namespace
-
-
-
-
