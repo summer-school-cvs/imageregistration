13910b7f064b6d95841bdb0a2a1a9501ddfeb815
Сысоенко Сергей <shkryab@gmail.com>
2020-10-11 04:04:55 +0300

    Тест фабрики разбит на 2 исполняемых файла

--- source/tests/factory_test2.cpp
+++ source/tests/factory_test2.cpp
@@ -1,9 +1,10 @@
 #include <gtest/gtest.h>
 #include <stitching/core/factory.h>
-#include <stitching/core/ifeaturing.h>
-#include <stitching/core/ihomography.h>
-#include <stitching/core/imatching.h>
-#include <stitching/core/iwarping.h>
+#include <stitching/featuring/orb/orb_featuring.h>
+#include <stitching/featuring/surf/surf_featuring.h>
+#include <stitching/homography/homography.h>
+#include <stitching/matching/knn_matching.h>
+#include <stitching/warping/warping.h>
 
 using stitching::core::IFeaturing;
 using stitching::core::IHomography;
@@ -20,9 +21,11 @@ DEFINE_TYPE(IHomography, Homography);
 DEFINE_TYPE(IMatcher, KnnMatcher);
 DEFINE_TYPE(IWarping, Warping);
 
+using namespace stitching;
+
 namespace {
 
-TEST(FactoryTest, create) {
+TEST(FactoryTest, create_check_types) {
   boost::property_tree::ptree config;
   auto surf    = Factory::create<IFeaturing, const bpt::ptree&>(IFeaturingSurfFeaturingKey, config);
   auto orb     = Factory::create<IFeaturing, const bpt::ptree&>(IFeaturingOrbFeaturingKey, config);
@@ -30,11 +33,11 @@ TEST(FactoryTest, create) {
   auto matcher = Factory::create<IMatcher>(IMatcherKnnMatcherKey);
   auto warping = Factory::create<IWarping>(IWarpingWarpingKey);
 
-  EXPECT_NE(nullptr, surf);
-  EXPECT_NE(nullptr, orb);
-  EXPECT_NE(nullptr, homo);
-  EXPECT_NE(nullptr, matcher);
-  EXPECT_NE(nullptr, warping);
+  EXPECT_NE(nullptr, std::dynamic_pointer_cast<featuring::SurfFeaturing>(surf));
+  EXPECT_NE(nullptr, std::dynamic_pointer_cast<featuring::OrbFeaturing>(orb));
+  EXPECT_NE(nullptr, std::dynamic_pointer_cast<homography::Homography>(homo));
+  EXPECT_NE(nullptr, std::dynamic_pointer_cast<matching::KnnMatching>(matcher));
+  EXPECT_NE(nullptr, std::dynamic_pointer_cast<warping::Warping>(warping));
 }
 
 }  // namespace
